73f94ee112adf84c09d334bfa00914f734f4323c
Charles Keyser <1chkeyser1@gmail.com>
2023-05-30 04:58:14 -0400

    feat(api-rentals): updated sample code to properly create structs

--- go.mod
+++ go.mod
@@ -11,6 +11,14 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/pgx/v4 v4.18.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
@@ -21,7 +29,7 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
-	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
--- main.go
+++ main.go
@@ -1,31 +1,130 @@
 package main
 
 import (
-	"net/http"
+	"database/sql"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 type Rental struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	ID              int     `json:"id"`
+	Name            string  `json:"name"`
+	Description     string  `json:"description"`
+	Type            string  `json:"type"`
+	Make            string  `json:"make"`
+	Model           string  `json:"model"`
+	Year            int     `json:"year"`
+	Length          float64 `json:"length"`
+	Sleeps          int     `json:"sleeps"`
+	PrimaryImageURL string  `json:"primary_image_url"`
+	Price           struct {
+		Day int `json:"day"`
+	} `json:"price"`
+	Location struct {
+		City    string  `json:"city"`
+		State   string  `json:"state"`
+		Zip     string  `json:"zip"`
+		Country string  `json:"country"`
+		Lat     float64 `json:"lat"`
+		Lng     float64 `json:"lng"`
+	} `json:"location"`
+	User struct {
+		ID        int    `json:"id"`
+		FirstName string `json:"first_name"`
+		LastName  string `json:"last_name"`
+	} `json:"user"`
+}
+
+func getRental(c *gin.Context, db *sql.DB) {
+	rental := Rental{
+		ID:       1,
+		Name:     "Sample Rental",
+		Price: struct {
+			Day int `json:"day"`
+		}{
+			Day: 100,
+		},
+		Location: struct {
+			City  string `json:"city"`
+			State string `json:"state"`
+		}{
+			City:  "Sample City",
+			State: "Sample State",
+		},
+	}
+
+	}
+
+	c.JSON(200, rental)
+}
+
+func getRentals(c *gin.Context, db *sql.DB) {
+	// Logic to retrieve and filter rentals based on the query parameters
+	// Use the db connection to fetch data from the PostgreSQL database
+	// Transform the data into a list of rental objects in the JSON structure
+	rentals := []Rental{
+		{
+			ID:        1,
+			Name:      "Sample Rental 1",
+			Price: struct {
+				Day int `json:"day"`
+			}{
+				Day: 100,
+			},
+			Location: struct {
+				City  string `json:"city"`
+				State string `json:"state"`
+			}{
+				City:  "Sample City 1",
+				State: "Sample State 1",
+			},
+		},
+		{
+			ID:        2,
+			Name:      "Sample Rental 2",
+			Price: struct {
+				Day int `json:"day"`
+			}{
+				Day: 200,
+			},
+			Location: struct {
+				City  string `json:"city"`
+				State string `json:"state"`
+			}{
+				City:  "Sample City 2",
+				State: "Sample State 2",
+			},
+		},
+	}
+
+
+	c.JSON(200, rentals)
 }
 
 func main() {
+	// Connect to the PostgreSQL database
+	db, err := sql.Open("pgx", "postgres://user:password@localhost:5432/database")
+	if err != nil {
+		log.Fatal("Failed to connect to the database:", err)
+	}
+	defer db.Close()
+
+	// Initialize the gin engine
 	router := gin.Default()
-	router.GET("/rentals/:id", getRental)
-	router.GET("/rentals", getRentals)
-	router.Run(":8080")
-}
 
-func getRental(c *gin.Context) {
-	rentalID := c.Param("id")
-	rental := Rental{ID: rentalID, Name: "Sample Rental"}
-	c.JSON(http.StatusOK, rental)
-}
+	// Define the routes and handlers
+	router.GET("/rentals/:rentalID", func(c *gin.Context) {
+		getRental(c, db)
+	})
+	router.GET("/rentals", func(c *gin.Context) {
+		getRentals(c, db)
+	})
 
-func getRentals(c *gin.Context) {
-	//location := c.Query("location")
-	rentals := []Rental{{ID: "1", Name: "Rental 1"}, {ID: "2", Name: "Rental 2"}}
-	c.JSON(http.StatusOK, rentals)
+	// Run the application
+	err = router.Run(":8080")
+	if err != nil {
+		log.Fatal("Failed to start the server:", err)
+	}
 }
